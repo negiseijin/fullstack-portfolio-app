d4d898aab14fdfd04e17cd621b6cf0e24f139a6e
negiseijin <satoru6221@gmail.com>
2024-04-09 21:46:31 +0900

    wip docker4

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,16 +1,19 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import books from "./books";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-const port = 3000
-console.log(`Server is running on port ${port}`)
+app.route("/books", books);
+
+const port = 3000;
+console.log(`Server is running on port ${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
