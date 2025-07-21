00b2ac21ec12fa5955ef8c4604df7e55f27192e0
negiseijin <satoru6221@gmail.com>
2025-07-21 11:26:38 +0000

    feat: add storybook

---
+++ apps/storybook/.storybook/preview.ts
@@ -0,0 +1,12 @@
+import '@repo/ui/styles.css'
+import { withThemeByClassName } from '@storybook/addon-themes'
+
+export const decorators = [
+  withThemeByClassName({
+    themes: {
+      light: 'light',
+      dark: 'dark',
+    },
+    defaultTheme: 'light',
+  }),
+]
